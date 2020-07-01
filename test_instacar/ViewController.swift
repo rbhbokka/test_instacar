5fbe8bfc78011b23ba0fd7896b58af65f5955a1f
kyemelyanenka <kiryl.yemelyanenka@epam.com>
2020-07-01 20:32:43 +0300

    Initial Commit

---
+++ test_instacar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_instacar
+//
+//  Created by Kirill Emelyanenko on 7/1/20.
+//  Copyright © 2020 bokka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
